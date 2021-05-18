5a99b91d825733e3f2e14e4990eaa3ec0588213e
João Luis Baraky <jlbaraky@gmail.com>
2021-05-17 21:39:19 -0300

    Adiciona arquivos do projeto

---
+++ main/gpio.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "gpio.h"
+#include "mqtt.h"
+#include "esp_sleep.h"
+//#include <string.h>
+
+
+void set_up_gpio() {
+    gpio_pad_select_gpio(BUTTON);
+    gpio_set_direction(BUTTON, GPIO_MODE_INPUT);
+    gpio_pulldown_en(BUTTON);
+    gpio_pullup_dis(BUTTON);
+
+    esp_sleep_enable_ext0_wakeup(BUTTON, 0);
+}
---
+++ main/gpio.h
@@ -0,0 +1,9 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#define BUTTON 0
+
+void input_handler(void *params);
+void set_up_gpio();
+
+#endif
